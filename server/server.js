2f2634d20f80c722f9e961a78445320b57614e54
Gaurav Tiwari <goodgauravtiwari@outlook.com>
2017-08-25 22:53:20 +0530

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 
 
@@ -39,6 +41,25 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 })
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id) ){
+    return res.status(404).send();
+  }
+
+  Todo.findbyId(id).then((todos) => {
+    if (!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todos});
+  }).catch((e) => {
+    res.status(400).send();
+  }
+});
 
 
 
